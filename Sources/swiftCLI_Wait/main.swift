a66a1c4e04bfe684dbfdd51686851ded6237c3f0
Tsungyu Yu <ytyubox@gmail.com>
2020-12-25 11:10:58 +0800

    run http request with run loop and exit

--- Sources/swiftCLI_Wait/main.swift
+++ Sources/swiftCLI_Wait/main.swift
@@ -1,1 +1,7 @@
-print("Hello, world!")
+import Foundation
+let url = URL(string: "https://www.apple.com/")!
+URLSession.shared.dataTask(with: url) { (data, response, error) in
+    print(String(data: data!, encoding: .utf8)!)
+    exit(EXIT_SUCCESS)
+}.resume()
+RunLoop.main.run()
