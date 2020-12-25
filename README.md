30d20a0d988977b91cbf9f3c7eabdccdddd9db7e
Tsungyu Yu <ytyubox@gmail.com>
2020-12-25 11:02:13 +0800

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# swiftCLI_Wait
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swiftCLI_WaitTests
+
+var tests = [XCTestCaseEntry]()
+tests += swiftCLI_WaitTests.allTests()
+XCTMain(tests)
